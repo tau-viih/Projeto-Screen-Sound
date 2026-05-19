1f2e59699e30da9b6555506af3b0ee3ca6029fb7
Tauane <tauanes26042002@gmail.com>
2026-05-18 22:50:16 -0300

    Iavaliavel e media das notas em musica.

--- ScreenSound/Modelos/Musica.cs
+++ ScreenSound/Modelos/Musica.cs
@@ -1,7 +1,9 @@
 ﻿namespace ScreenSound.Modelos;
 
-internal class Musica
+internal class Musica : IAvaliavel
 {
+    private List<Avaliacao> notas = new();
+
     public Musica(Banda artista, string nome)
     {
         Artista = artista;
@@ -13,6 +15,20 @@ internal class Musica
     public int Duracao { get; set; }
     public bool Disponivel { get; set; }
     public string DescricaoResumida => $"A música {Nome} pertence à banda {Artista}";
+
+    public double Media
+    {
+        get
+        {
+            if (notas.Count == 0) return 0;
+            else return notas.Average(a => a.Nota);
+        }
+    }
+
+    public void AdicionarNota(Avaliacao nota)
+    {
+        notas.Add(nota);
+    }
 
     public void ExibirFichaTecnica()
     {
